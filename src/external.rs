8094923213b34f6df47d5762e5a22942aba42bec
McKayla Washburn <mckayla@hey.com>
2023-07-14 21:09:02 -0600

    more tweaks

--- src/external.rs
+++ src/external.rs
@@ -1,12 +1,10 @@
 use chrono::NaiveDate;
-use serde::ser::SerializeStruct;
 use serde::Deserialize;
-use serde::Deserializer;
 use serde::Serialize;
-use serde::Serializer;
 use std::cmp::Ordering;
 
 use crate::pocky::de;
+use crate::pocky::ser;
 
 #[derive(Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub struct External {
@@ -27,40 +25,14 @@ impl Ord for External {
 	}
 }
 
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Serialize, Eq, PartialEq)]
 pub struct ExternalMetadata {
 	pub title: String,
 	pub author: String,
-	#[serde(default, deserialize_with = "de_date")]
+	#[serde(deserialize_with = "de::date", serialize_with = "ser::date_display")]
 	pub date: NaiveDate,
 	#[serde(default, deserialize_with = "de::markdown_option")]
 	pub summary: Option<String>,
 	#[serde(default, deserialize_with = "de::comma_separated")]
 	pub tags: Vec<String>,
-}
-
-impl Serialize for ExternalMetadata {
-	fn serialize<S>(&self, ser: S) -> Result<S::Ok, S::Error>
-	where
-		S: Serializer,
-	{
-		let mut state = ser.serialize_struct("ExternalMetadata", 5)?;
-		state.serialize_field("title", &self.title)?;
-		state.serialize_field("author", &self.author)?;
-		state.serialize_field("date", &self.date.format("%A, %B %-d, %Y").to_string())?;
-		state.serialize_field("summary", &self.summary)?;
-		state.serialize_field("tags", &self.tags)?;
-		state.end()
-	}
-}
-
-fn de_date<'de, D>(de: D) -> Result<NaiveDate, D::Error>
-where
-	D: Deserializer<'de>,
-{
-	let date_string = String::deserialize(de)?;
-	let date = NaiveDate::parse_from_str(&date_string, "%Y.%m.%d")
-		.unwrap_or_else(|_| panic!("invalid date: {}", date_string));
-
-	Ok(date)
 }
